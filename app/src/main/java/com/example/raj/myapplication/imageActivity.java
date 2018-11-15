810d776bc6a024f3c2b7d298968cf1416c0c0183
Raj Das <mail.rajdas@gmail.com>
2018-11-15 12:58:39 +0530

    changes

--- app/src/main/java/com/example/raj/myapplication/imageActivity.java
+++ app/src/main/java/com/example/raj/myapplication/imageActivity.java
@@ -10,21 +10,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class imageActivity extends AppCompatActivity {
 
-    Button b1, b2, b3;
-    ImageView imageView;
+    Button b1, b2, b3, b4, b5, b6;
+    ImageView imageView, imageView2;
+    Bitmap bm1, bm2;
+    EditText URL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
         imageView = findViewById(R.id.imgv1);
+        imageView2 = findViewById(R.id.imgv2);
         b1 = (Button) findViewById(R.id.cam);
         b2 = (Button) findViewById(R.id.map);
         b3 = (Button) findViewById(R.id.phone);
+        b4 = (Button) findViewById(R.id.cam2);
+        b5 = (Button) findViewById(R.id.swap);
+        b6 = (Button) findViewById(R.id.browser);
+
+        URL= findViewById(R.id.url);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,12 +64,53 @@ public class imageActivity extends AppCompatActivity {
 
             }
         });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,2);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap c;
+                c = bm1;
+                bm1 = bm2;
+                bm2 = c;
+
+                imageView.setImageBitmap(bm2);
+                imageView2.setImageBitmap(bm1);
+
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri url = Uri.parse("http://"+ URL.getText());
+                Intent i = new Intent(Intent.ACTION_VIEW, url);
+                startActivity(i);
+            }
+        });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        Bitmap bm = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(bm);
+        if(requestCode == 1){
+            bm1 = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bm1);
+        }
+
+        else if(requestCode == 2){
+            bm2 = (Bitmap) data.getExtras().get("data");
+            imageView2.setImageBitmap(bm2);
+        }
+
     }
 }
